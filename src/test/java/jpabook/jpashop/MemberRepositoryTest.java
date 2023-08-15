2d7a25847dae1982a1d2774098359f7051cfe65c
jeounpar <cdex6531@ajou.ac.kr>
2023-08-15 21:49:14 +0900

    MemberRepository 생성

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -13,24 +13,4 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class MemberRepositoryTest {
 
-	@Autowired
-	MemberRepository memberRepository;
-
-	@DisplayName("멤버를 하나 생성해서 데이터베이스에 저장한다")
-	@Test
-	@Transactional
-	void memberSave() {
-		// given
-		Member member = new Member();
-		member.setUsername("memberA");
-
-		// when
-		Long saveId = memberRepository.save(member);
-		Member findMember = memberRepository.find(saveId);
-
-		//then
-		assertThat(saveId).isEqualTo(findMember.getId());
-		assertThat(member.getUsername()).isEqualTo(findMember.getUsername());
-		assertThat(findMember).isEqualTo(member);
-	}
 }
