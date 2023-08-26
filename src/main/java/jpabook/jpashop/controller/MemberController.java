fab9de97d78b44622e25d337247b2e501d2616ac
jeounpar <cdex6531@ajou.ac.kr>
2023-08-26 16:23:43 +0900

    commit

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -4,6 +4,7 @@ import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,7 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
@@ -24,6 +27,13 @@ public class MemberController {
 		return "members/createMemberForm";
 	}
 
+	@GetMapping("/members")
+	public String list(Model model) {
+		List<Member> members = memberService.findMembers();
+		model.addAttribute("members", members);
+		return "members/memberList";
+	}
+
 	@PostMapping("/members/new")
 	public String create(@Valid MemberForm memberForm, BindingResult result) {
 
@@ -34,9 +44,8 @@ public class MemberController {
 		Address address = new Address(memberForm.getCity(), memberForm.getStreet(), memberForm.getZipcode());
 
 		Member member = new Member();
-		member.setName(member.getName());
+		member.setName(memberForm.getName());
 		member.setAddress(address);
-
 		memberService.join(member);
 		return "redirect:/";
 	}
